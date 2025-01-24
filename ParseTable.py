5cfeef852b9fecfd09a58466d916f11d5c588851
123100123 <146941456+123100123@users.noreply.github.com>
2025-01-24 17:52:31 +0330

    CFG
    - bug fixes

--- ParseTable.py
+++ ParseTable.py
@@ -13,17 +13,16 @@ class CFG:
         if isinstance(production,str):
             self.rules[variable].extend(production.split())
         elif isinstance(production,list):
-            self.productions[variable].extend([p.split() for p in production])
+            self.rules[variable].extend([p.split() for p in production])
 
 
     def display(self):
-        for variable, rules in self.productions.items():
+        for variable, rules in self.rules.items():
             rules_str = " | ".join([" ".join(rule) for rule in rules])
             print(f"{variable} -> {rules_str}")
 
 
     def fill(self):
-        self.set_start_symbol("Start")
         self.add_production("Start", ["S N M"])
         self.add_production("S", ["#include S", "ε"])
         self.add_production("N", ["using namespace std ;", "ε"])
@@ -46,4 +45,4 @@ class CFG:
         self.add_production("C", ['"sum="', "number", "string", "identifier"])
         self.add_production("H", ["<< C H", "ε"])
         self.add_production("V", ["return 0 ;", "ε"])
-    
+
